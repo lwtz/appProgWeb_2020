24b34949dc88673a71c2e8143364b3742a8f627c
Ronei Ely <roneiely@hotmail.com>
2020-05-31 10:46:57 -0300

    Adicionando adminLTE

--- routes/web.php
+++ routes/web.php
@@ -17,3 +17,9 @@ use Illuminate\Http\Request;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Auth::routes();
+
+Route::get('/home', function() {
+    return view('home');
+})->name('home')->middleware('auth');
